b1374fc5a123e2c9be2883fc35cee641b89406ad
soutag <tagcutz@gmail.com>
2025-06-04 02:09:34 +0200

    added actions

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://tagsou.github.io',
+   base: '/mysite'
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'astro-erudite',
+  title: 'sou_tag',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
+    'todoooo',
+  href: '',
+  author: 'sou_tag',
   locale: 'en-US',
   featuredPostCount: 2,
   postsPerPage: 3,
@@ -16,10 +16,7 @@ export const NAV_LINKS: SocialLink[] = [
     href: '/blog',
     label: 'blog',
   },
-  {
-    href: '/authors',
-    label: 'authors',
-  },
+
   {
     href: '/about',
     label: 'about',
@@ -28,28 +25,19 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/tagsou',
     label: 'GitHub',
   },
   {
-    href: 'https://twitter.com/enscry',
+    href: '',
     label: 'Twitter',
   },
-  {
-    href: 'mailto:jason@enscribe.dev',
-    label: 'Email',
-  },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
-  },
+ 
+
 ]
 
 export const ICON_MAP: IconMap = {
   Website: 'lucide:globe',
   GitHub: 'lucide:github',
-  LinkedIn: 'lucide:linkedin',
   Twitter: 'lucide:twitter',
-  Email: 'lucide:mail',
-  RSS: 'lucide:rss',
 }
